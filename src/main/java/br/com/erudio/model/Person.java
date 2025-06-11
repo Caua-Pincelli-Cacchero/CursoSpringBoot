aa1d0b404020ef953c3e32099f0ce9dc97575baa
Cauã Pincelli Cacchero <cauapincacchero@gmail.com>
2025-06-11 17:40:49 -0300

    Injeção de dependencias

---
+++ src/main/java/br/com/erudio/model/Person.java
@@ -0,0 +1,68 @@
+package br.com.erudio.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private long id;
+    private String firsName;
+    private String lastName;
+    private String address;
+    private String gender;
+
+    public Person() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirsName() {
+        return firsName;
+    }
+
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Person person)) return false;
+        return getId() == person.getId() && Objects.equals(getFirsName(), person.getFirsName()) && Objects.equals(getLastName(), person.getLastName()) && Objects.equals(getAddress(), person.getAddress()) && Objects.equals(getGender(), person.getGender());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirsName(), getLastName(), getAddress(), getGender());
+    }
+}
